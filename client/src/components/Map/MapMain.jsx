a5dd457a4a681eaaf5cc8179877fb1e03ba92905
Taylor Bantle <bantle.taylor@gmail.com>
2019-03-18 19:25:33 -0700

    App conditionally renders page and started to set up map files

---
+++ client/src/components/Map/MapMain.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import mapConfig from './mapconfig.js';
+import GoogleMaps from 'google-maps';
+import  axios from 'axios';
+
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    //function to get  
+
+    render() {
+        return (
+            <div>Map</div>
+        )
+    }
+}
+
+export default Main;
