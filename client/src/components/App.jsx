37f4844a933d2841069bf8dd43b681b025c5745c
Taylor Bantle <bantle.taylor@gmail.com>
2019-03-18 19:25:33 -0700

    App conditionally renders page and started to set up map files

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,41 @@
 import React from 'react';
 import styles from './AppStyles.css';
 
+import Map from './Map/MapMain.jsx';
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state  =  {
-
+            page: 1
         }
+        this.changePage = this.changePage.bind(this);
     }
 
+    //conditionally rendering correct page
+    changePage() {
+        let newPage =  this.state.page+1;
+        this.setState({page: newPage})
+    }
+
+    //this should render the correct page based on whether someone is  signed in (signup page vs homepage)
     render()  {
-        return (
-            <div className={styles.title}>ParkShare</div>
-        )
+        let { page } = this.state;
+        if (page === 1) {
+            return (
+                <div>
+                    <div className={styles.title}>ParkShare</div>
+                    <button onClick={this.changePage}>Go to map</button>
+                </div>
+            )
+        } else if (page  === 2)  {
+            return (
+            <div>
+                <div className={styles.title}>ParkShare</div>
+                <Map />
+            </div>
+            )
+        }
     }
 }
 
