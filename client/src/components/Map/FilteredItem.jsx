85eefdc1f6160f7b250cefda1570ceb2af23f708
Taylor Bantle <bantle.taylor@gmail.com>
2019-03-20 13:31:08 -0700

    Ability to filter spaces based on geolocation of user

--- client/src/components/Map/FilteredItem.jsx
+++ client/src/components/Map/FilteredItem.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const FilteredItem = (props) => {
-    // console.log(props.item)
     return (
         <button>{props.item} <span id={props.item} onClick={(e) => props.removeFilter(e)}>x</span></button>
     )
