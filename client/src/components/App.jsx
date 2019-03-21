dd041af80c86369c32a6623b23d3e0b5c80a2871
Caroline Cange <carolinecang@gmail.com>
2019-03-21 00:28:59 -0700

    init sign up

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './AppStyles.css';
-
+import SignUp from './SignUp/SignMain.jsx';
 import Map from './Map/MapMain.jsx';
 
 class App extends React.Component {
@@ -23,17 +23,18 @@ class App extends React.Component {
         let { page } = this.state;
         if (page === 1) {
             return (
-                <div>
+                <div className={styles.backgrounds}>
                     <div className={styles.title}>ParkShare</div>
+                    <SignUp />
                     <button onClick={this.changePage}>Go to map</button>
                 </div>
             )
         } else if (page  === 2)  {
             return (
-            <div>
-                <div className={styles.title}>ParkShare</div>
-                <Map />
-            </div>
+              <div>
+                  <div className={styles.title}>ParkShare</div>
+                  <Map />
+              </div>
             )
         }
     }
---
+++ client/src/components/SignUp/SignMain.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./SignUp.css";
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.state = {
+    //   map: ''
+    // }
+  }
+
+  // componentDidMount() {
+  // }
+
+  render() {
+    return (
+      <div>
+
+        <div>
+         
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SignUp;
