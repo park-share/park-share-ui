85f6370961bb2f5fa5bcc4f2e025a364091f7813
KweonMo Koo <mikekoo2010@gmail.com>
2019-03-22 09:33:57 -0700

    Create Stripe checkout form

--- client/src/components/Checkout/CheckoutForm.jsx
+++ client/src/components/Checkout/CheckoutForm.jsx
@@ -2,38 +2,14 @@ import React, {Component} from 'react';
 import {CardElement, injectStripe} from 'react-stripe-elements';
 import axios from 'axios';
 
-class CheckoutForm extends Component {
-  constructor(props) {
-    super(props);
-    this.submit = this.submit.bind(this);
-  }
-
-  async submit(ev) {
-    let {token} = await this.props.stripe.createToken({name: "Name"});
-
-    axios
-      .post('/charge')
-      .then(() => {
-        console.log("Purchase Complete!")
-      })
-    let response = await fetch("/charge", {
-      method: "POST",
-      headers: {"Content-Type": "text/plain"},
-      body: token.id
-    });
-
-    if (response.ok) console.log("Purchase Complete!");
-  }
-
-  render() {
+const CheckoutForm = (props) => {
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
         <CardElement />
-        <button onClick={this.submit}>Send</button>
+        <button onClick={props.submit}>Send</button>
       </div>
     );
-  }
 }
 
 export default injectStripe(CheckoutForm);
