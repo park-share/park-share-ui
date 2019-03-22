13241867fedb101ceb0d43889b0bad1586ae74cb
Caroline Cange <carolinecang@gmail.com>
2019-03-21 21:34:32 -0700

    shoud be able to redirect to the map page

--- client/src/components/SignUp/Parent.jsx
+++ client/src/components/SignUp/Parent.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./SignUp.css";
 import SignUp from "./SignMain.jsx";
 import Login from "./LogIn.jsx";
-// var createReactClass = require("create-react-class");
 
 
 class Parent extends React.Component {
@@ -38,75 +37,11 @@ class Parent extends React.Component {
         <p id="loginButton" onClick={self.switch.bind(null, "login")} className={self.state.login ? "yellow" : "blue"}> Login</p>
       </div>
 
-      {self.state.signup ? <SignUp /> : null}
-      {self.state.login ? <Login /> : null}
+      {self.state.signup ? <SignUp changePage={this.props.changePage}/> : null}
+      {self.state.login ? <Login changePage={this.props.changePage}/> : null}
 
     </div>
 
-  )
-
-
-}
-  // render() {
-  //   return (
-  //     <div>
-  //       <button onClick={this.switch}>Sign Up</button>
-  //     </div>
-  //   );
-  // }
-  // render() {
-  //   return (
-  //     <div>
-  //       <div id="buttons">
-  //         <p
-  //           id="signupButton"
-  //           onClick={this.switch(null, "signup")}
-  //           // onClick={self.switch(null, "signup")}
-  //           // className={self.state.signup ? "yellow" : "blue"}
-  //         >
-  //           Sign In
-  //         </p>
-  //         <p
-  //           id="loginButton"
-  //           onClick={this.switch(null, "login")}
-  //           // className={self.state.login ? "yellow" : "blue"}
-  //         >
-  //           {" "}
-  //           Login
-  //         </p>
-  //       </div>
-
-  //       {/* {self.state.signup ? <Signup /> : null} */}
-  //       {/* {self.state.login ? <Login /> : null} */}
-  //     </div>
-  //   );
-  // }
+  )}
 }
 export default Parent;
-
-
-// var Parent = React.createClass({
-//   getInitialState: function () {
-//     return { signup: false, login: true }
-//   },
-  
-//   render: function () {
-
-//     var self = this;
-//     return (
-//       <div>
-//         <div id="buttons">
-//           <p id="signupButton" onClick={self.switch.bind(null, "signup")} className={self.state.signup ? "yellow" : "blue"}>Sign In</p>
-//           <p id="loginButton" onClick={self.switch.bind(null, "login")} className={self.state.login ? "yellow" : "blue"}> Login</p>
-//         </div>
-
-//         {self.state.signup ? <Signup /> : null}
-//         {self.state.login ? <Login /> : null}
-
-//       </div>
-
-//     )
-
-
-//   }
-// })
