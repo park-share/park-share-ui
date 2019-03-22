490c6c602ed10e6cb71e661181106dde19f80217
Taylor Bantle <bantle.taylor@gmail.com>
2019-03-21 21:19:19 -0700

    Added function to reserve space for reservation component

--- client/src/components/Map/MarkerInfo.jsx
+++ client/src/components/Map/MarkerInfo.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const MarkerInfo = (props) => {
-    let {selectedPlace} = props;
-    console.log(selectedPlace.spot)
+    let {selectedPlace, handleReserve} = props;
+    // console.log(selectedPlace.spot)
     return (
         <div>
             <h2>{selectedPlace.name}</h2>
             <div>{selectedPlace.title}</div>
             {/* <div>Weekday Rate: ${selectedPlace.spot.weekday_rate}/day</div>
             <div>Weekend Rate: ${selectedPlace.spot.weekend_rate}/day</div> */}
-            <button>Reserve Space</button>
+            <button onClick={(e) => handleReserve(e, selectedPlace.spot)}>Reserve Space</button>
         </div>
     )
 }
