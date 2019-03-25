fef6d50ff27e77d6ececb698d140bb546f8b00fa
Taylor Bantle <bantle.taylor@gmail.com>
2019-03-25 09:09:38 -0700

    Updated button styling and responsive height

--- client/src/components/Map/MapListings.jsx
+++ client/src/components/Map/MapListings.jsx
@@ -6,10 +6,10 @@ const Listings = (props) => {
     let top100 = props.filteredSpots.slice(0, 100);
     return (
         <div >
-            <h3>Spots:</h3>
+            <h3>Nearby Parking Spots:</h3>
             <div className={styles.listingsContainer}>{top100.map((spot, i) => {
                 return <MapListing spot={spot} key={i} handleReserve={props.handleReserve} />
-            })}</div>
+            })}</div><br/>
             <div>Showing {top100.length} of {props.filteredSpots.length} available spots</div>
         </div>
         
--- client/src/components/Map/MapMarkers.jsx
+++ client/src/components/Map/MapMarkers.jsx
@@ -3,7 +3,6 @@ import {Marker} from 'google-maps-react';
 
 const MapMarker = (props) => {
     let { spot } = props;
-    console.log(spot)
     return (
         <Marker name={spot.name} title={spot.parking_address} position={{lat: spot.latitudes, lng: spot.longitudes}} />
     )
